3159485a63439afaf249c114b06b97bf389658b1
rissa <rissa@FELIPE>
2024-05-16 00:03:29 -0300

    botao teste

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.teste = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // teste
+            // 
+            this.teste.Location = new System.Drawing.Point(356, 258);
+            this.teste.Name = "teste";
+            this.teste.Size = new System.Drawing.Size(137, 70);
+            this.teste.TabIndex = 0;
+            this.teste.Text = "teste";
+            this.teste.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.teste);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button teste;
     }
 }
 
