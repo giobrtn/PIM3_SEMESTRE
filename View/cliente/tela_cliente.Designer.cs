ef183975992eae8062da483cd1adbc2a31d3ebad
rissa <rissa@FELIPE>
2024-06-05 23:32:26 -0300

    teste estilização datagrid

--- View/cliente/tela_cliente.Designer.cs
+++ View/cliente/tela_cliente.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button_editar = new System.Windows.Forms.Button();
             this.button_adicionar = new System.Windows.Forms.Button();
@@ -102,6 +103,14 @@
             // dataGridView_clientes
             // 
             this.dataGridView_clientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView_clientes.DefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView_clientes.Location = new System.Drawing.Point(47, 83);
             this.dataGridView_clientes.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView_clientes.Name = "dataGridView_clientes";
